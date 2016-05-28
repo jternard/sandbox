3c6e5b2a59961e4436b161ffd4885608ff312d50
jeremy <jeremy.ternard@gmail.com>
2016-05-28 03:57:19 +0200

    uploading first real commit

---
+++ NavigoTest/app/src/main/java/jeremyternard/navigotest/utils.java
@@ -0,0 +1,10 @@
+package jeremyternard.navigotest;
+
+/**
+ * Created by Jeremy on 28/05/2016.
+ */
+public class utils {
+    public String firstFunction(){
+        return "Hello World";
+    }
+}
